6a59cb9a5aeb7bf5d57c90049b1df93c7351eda4
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2021-07-12 13:14:09 +0100

    Stored project in singular folder

---
+++ project/static/js/base.js
@@ -0,0 +1,20 @@
+function responsiveNav() {
+  if (window.innerWidth < 960){
+    window.addEventListener("load", (event) => {
+      document.querySelector(".nav").style.display = "none";
+      document.querySelector(".nav-mobile").style.display = "block";
+    })
+  }
+}
+
+function fixedNav() {
+  const nav = document.querySelector("nav")
+
+  window.addEventListener("scroll", (event) => {
+    let position = window.scrollY > 100
+    nav.classList.toggle("nav-fixed", position)
+  })
+}
+
+fixedNav()
+responsiveNav()
---
+++ project/static/js/product.js
@@ -0,0 +1,142 @@
+function detailReveal() {
+  // Retrieve what coffee user has asked for
+  document.querySelectorAll(".nav-img-cont").forEach((navDiv) => {
+    navDiv.addEventListener("click", (event) => {
+      let userPicked = event.target
+
+      if(userPicked.className.split(" ")[0] == "side-nav-title") {
+        let fatherDiv = userPicked.parentElement
+        let textElement = userPicked
+
+        let allFatherDiv = document.querySelectorAll(".nav-img-cont").forEach((div) => {
+          div.style.background = "#454545"
+        })
+
+        let allTextElement = document.querySelectorAll(".side-nav-title").forEach((text) => {
+          text.style.color = "#ECD7BC"
+        })
+
+        fatherDiv.style.background = "#EEEEEE"
+        textElement.style.color = "#454545"
+        
+        userPicked = userPicked.className.split(" ")[1].split("-")[1]
+      }
+
+      else {
+        let fatherDiv = userPicked
+        let textElement = userPicked.children[0]
+
+        let allFatherDiv = document.querySelectorAll(".nav-img-cont").forEach((div) => {
+          div.style.background = "#454545"
+        })
+
+        let allTextElement = document.querySelectorAll(".side-nav-title").forEach((text) => {
+          text.style.color = "#ECD7BC"
+        })
+
+        fatherDiv.style.background = "#EEEEEE"
+        textElement.style.color = "#454545"
+
+        userPicked = userPicked.className.split(" ")[0]
+      }
+
+      let current = document.querySelectorAll(".option-details").forEach((x) => {
+        x.classList.add("option-deactive")
+      })
+
+      const newDetail = document.querySelector(`.d-${userPicked}`).parentElement
+      newDetail.classList.remove("option-deactive")
+    })
+  })
+  // End of retrieve
+}
+
+function detailCoffeeMakerReveal() {
+  // Retrieve what coffee user has asked for
+  document.querySelectorAll(".nav-img-cont-coffee").forEach((navDiv) => {
+    navDiv.addEventListener("click", (event) => {
+      let userPicked = event.target
+
+      if(userPicked.className.split(" ")[0] == "side-nav-title-coffee") {
+        let fatherDiv = userPicked.parentElement
+        let textElement = userPicked
+
+        let allFatherDiv = document.querySelectorAll(".nav-img-cont-coffee").forEach((div) => {
+          div.style.background = "#454545"
+        })
+
+        let allTextElement = document.querySelectorAll(".side-nav-title-coffee").forEach((text) => {
+          text.style.color = "#ECD7BC"
+        })
+
+        fatherDiv.style.background = "#EEEEEE"
+        textElement.style.color = "#454545"
+        
+        userPicked = userPicked.className.split(" ")[1].split("-")[1]
+      }
+
+      else {
+        let fatherDiv = userPicked
+        let textElement = userPicked.children[0]
+
+        let allFatherDiv = document.querySelectorAll(".nav-img-cont-coffee").forEach((div) => {
+          div.style.background = "#454545"
+        })
+
+        let allTextElement = document.querySelectorAll(".side-nav-title-coffee").forEach((text) => {
+          text.style.color = "#ECD7BC"
+        })
+
+        fatherDiv.style.background = "#EEEEEE"
+        textElement.style.color = "#454545"
+
+        userPicked = userPicked.className.split(" ")[0]
+      }
+
+      let current = document.querySelectorAll(".option-details-coffee").forEach((x) => {
+        x.classList.add("option-deactive")
+      })
+
+      const newDetail = document.querySelector(`.d-${userPicked}`).parentElement
+      newDetail.classList.remove("option-deactive")
+    })
+  })
+  // End of retrieve
+}
+
+function pagination() {
+  let leftCirc = document.querySelector(".first-circ");
+  let rightCirc = document.querySelector(".last-circ");
+  let activeCirc = document.querySelector(".active-circ");
+
+  let firstSet = document.querySelector(".first-set");
+  let secondSet = document.querySelector(".second-set");
+
+  leftCirc.addEventListener("click", e => {
+    if(leftCirc != activeCirc) {
+      console.log("Test");
+    }
+
+    else{
+      rightCirc.classList.toggle("active-circ");
+      leftCirc.classList.toggle("active-circ");
+
+      firstSet.classList.toggle("nav-deactive")
+      secondSet.classList.toggle("nav-deactive")
+    }
+  })
+
+  rightCirc.addEventListener("click", e => {
+    if(rightCirc != activeCirc) {
+      rightCirc.classList.toggle("active-circ");
+      leftCirc.classList.toggle("active-circ");
+
+      firstSet.classList.toggle("nav-deactive")
+      secondSet.classList.toggle("nav-deactive")
+    }
+  })
+}
+
+pagination()
+detailReveal()
+detailCoffeeMakerReveal()
